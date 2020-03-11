c827f1e0d75d974d09fad91a42911ed06bb1389c
AsioSir <3235254126@qq.com>
2020-03-11 14:47:21 +0800

    更新3

--- src/script/base.js
+++ src/script/base.js
@@ -44,7 +44,6 @@ function Base() {
      * 初始化
      */
     this.init = function () {
-        require(['JQuery']);
         bndongJs.loadingBeforeInit(); // Loading 前初始化
         bndongJs.endLoading();        // Loading 结束
         bndongJs.loadingAfterInit();  // Loading 后初始化
