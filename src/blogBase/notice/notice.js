6795178461b2a722c0bc7debee38e59a2221865d
AsioSir <46233287+AsioSir@users.noreply.github.com>
2020-04-03 18:12:26 +0800

    Update notice.js

--- src/blogBase/notice/notice.js
+++ src/blogBase/notice/notice.js
@@ -1,22 +1,22 @@
-$(document).ready(function(){
+window.onload = function(){
     var anim = "fadeInDown";
     var removeanim = "fadeOutUp"
+    document.getElementById("notice_card").style.opacity = 1;
     init()
     function init() {
         var date = new Date();
         var nowtime = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDay() + " " + date.toLocaleTimeString('chinese',{hour12:false});
-        document.getElementById("time").innerText = nowtime;
-        console.log(nowtime)
+        document.getElementById("notice_time").innerText = nowtime;
         testAnim(anim)
         animTimer();
     }
     function testAnim(x) {
-        $('#card').removeClass().addClass(x + ' animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
+        $('#notice_card').removeClass().addClass(x + ' animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
     };
 
     function animTimer(){
         setTimeout(function () {
             testAnim(removeanim);
-        },2500)
+        },3000)
     }
-});
+};
